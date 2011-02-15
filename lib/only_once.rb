24484b8e6ec60595023228f053c06a8033b5083a
katmagic <the.magical.kat@gmail.com>
2011-02-15 00:25:53 -0500

    Add a warning about our memory leak.

--- lib/only_once.rb
+++ lib/only_once.rb
@@ -2,7 +2,9 @@
 
 # Do something, but only once (in the lifetime of the program). This is based on
 # the output of caller() and the value of _args_, so there's some possibility
-# for error if you've reloaded files, etc.
+# for error if you've reloaded files, etc. Additionally, you should be strongly
+# cautioned that objects given as arguments to only_once() will persist in
+# memory until program termination.
 def only_once(*args)
 	$only_once_previously_called_from ||= Array.new
 	unless $only_once_previously_called_from.include? [caller[0], args]
